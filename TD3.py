655230b852da4a0acf7fec4e0b6a3806fa2fae25
Raagheivan SIVAPALAN <raagheivan.sivapalan@ens.uvsq.fr>
2022-09-30 19:23:18 +0200

    Progression

--- TD3.py
+++ TD3.py
@@ -1,12 +1,15 @@
 import time
 
+
 def tempsEnSecondes(temps):
-    """ Renvoie la valeur en seconde de temps donné comme jour, heure, minute, seconde."""
+    """ Renvoie la valeur en seconde de temps donné comme\
+    jour, heure, minute, seconde."""
     return temps[0] * 24 * 3600 + temps[1] * 3600 + temps[2] * 60 + temps[3]  
 
 
 def secondeEnTemps(secondes):
-    """Renvoie le temps (jour, heure, minute, seconde) qui correspond au nombre de seconde passé en argument"""
+    """Renvoie le temps (jour, heure, minute, seconde) qui correspond \
+        au nombre de seconde passé en argument"""
     j = secondes // (24 * 3600)
     h = secondes % (24 * 3600) // 3600
     min = (secondes % (24 * 3600)) % 3600 // 60
@@ -14,28 +17,24 @@ def secondeEnTemps(secondes):
     return (j, h, min, secondes)
 
 
-
-
-
 def afficheTemps(temps):
     """ Renvoie l'affichage du temps """
     if temps[0] == 1:
-        print(temps[0], "jour, ", end = "")
+        print(temps[0], "jour, ", end="")
     elif temps[0] > 1:
-        print(temps[0], "jours, ", end = "")
+        print(temps[0], "jours, ", end="")
     if temps[1] == 1:
-        print(temps[1], "heure, ", end = "")
+        print(temps[1], "heure, ", end="")
     elif temps[1] > 1:
-        print(temps[1], "heures, ", end = "")
+        print(temps[1], "heures, ", end="")
     if temps[2] == 1:
-        print(temps[2], "minute, ", end = "")
+        print(temps[2], "minute, ", end="")
     elif temps[2] > 1:
-        print(temps[2], "minutes, ", end = "")
+        print(temps[2], "minutes, ", end="")
     if temps[3] == 1:
-        print(temps[3], "seconde, ", end = "")
+        print(temps[3], "seconde, ", end="")
     elif temps[3] > 1:
-        print(temps[3], "secondes, ", end = "")
-
+        print(temps[3], "secondes, ", end="")
 
 
 def demandeTemps():
@@ -52,16 +51,12 @@ def demandeTemps():
     return(int(j), int(h), int(m), int(s))
 
 
-
-
-def sommeTemps(temps1,temps2):
+def sommeTemps(temps1, temps2):
     return secondeEnTemps(tempsEnSecondes(temps1) + tempsEnSecondes(temps2))
 
 
-def proportionTemps(temps = (0, 0, 0, 0) ,proportion = 0.1):
+def proportionTemps(temps=(0, 0, 0, 0), proportion=0.1):
     return secondeEnTemps(tempsEnSecondes(temps) * proportion)
-
-
 
 
 def tempsEndate(temps):
@@ -76,37 +71,59 @@ def tempsEndate(temps):
 def afficheDate(temps):
     t = temps
     if t[1] < 31:
-        print(t[1], "Janvier", end = "")
+        print(t[1], "Janvier", end="")
     elif t[1] < 59:
-        print(t[1] - 31, "Février", end = "")
+        print(t[1] - 31, "Février", end="")
     elif t[1] < 90:
-        print(t[1] - 59, "Mars", end = "")
+        print(t[1] - 59, "Mars", end="")
     elif t[1] < 120:
-        print(t[1] - 90, "Avril", end = "")
+        print(t[1] - 90, "Avril", end="")
     elif t[1] < 151:
-        print(t[1] - 120, "Mai", end = "")
+        print(t[1] - 120, "Mai", end="")
     elif t[1] < 181:
-        print(t[1] - 151, "Juin", end = "")
+        print(t[1] - 151, "Juin", end="")
     elif t[1] < 212:
-        print(t[1] - 181, "Juillet", end = "")
+        print(t[1] - 181, "Juillet", end="")
     elif t[1] < 242:
-        print(t[1] - 212, "Août", end = "")
+        print(t[1] - 212, "Août", end="")
     elif t[1] < 273:
-        print(t[1] - 242, "Septembre", end = "")
+        print(t[1] - 242, "Septembre", end="")
     elif t[1] < 303:
-        print(t[1] - 273, "Octobre", end = "")
+        print(t[1] - 273, "Octobre", end="")
     elif t[1] < 334:
-        print(t[1] - 303, " Novembre", end = "")
+        print(t[1] - 303, " Novembre", end="")
     elif t[1] < 364:
-        print(t[1] - 334, "Décembre", end = "")
-    print("",t[0], "à", t[2], ":", t[3], ": ", t[4])
+        print(t[1] - 334, "Décembre", end="")
+    print("", t[0], "à", t[2], ":", t[3], ": ", t[4])
+
+
+def bisextile(jour):
+    ans = jour // 365
+    for i in range(ans + 1):
+        if (1970 + i) % 4 == 0 and ((1970 + i) % 100 != 0 or
+                                    (1970 + i) % 400 == 0):
+            print(1970 + i, ", ", end="")
+
+
+def nombreBisextile(jour):
+    n = 0
+    ans = jour // 365
+    for i in range(ans + 1):
+        if (1970 + i) % 4 == 0 and ((1970 + i) % 100 != 0 or (1970 + i) % 400 == 0):
+            n += 1
+    return n
+
+
+def tempsEnDateBisextile(temps):
+    t = tempsEnSecondes(temps)
+    t = secondeEnTemps(t)
+    b = nombreBisextile(t[0])
+    ans = 1970 + temps[0] // 365
+    jour = temps[0] % 365 - b
+    return (ans, jour, temps[1], temps[2], temps[3])
+
 
 temps = secondeEnTemps(1000000000)
 afficheTemps(temps)
-temps = tempsEndate(temps)
-print(temps)
-afficheDate(temps)
-
-
-print(time.gmtime(1000000000))
-print(time.localtime(1000000000))
+afficheDate(tempsEnDateBisextile(temps))
+print(time.gmtime())
